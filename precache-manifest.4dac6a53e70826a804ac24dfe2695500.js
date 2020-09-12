bf8c44d4a85e2246e7d84a8ed91e66ab79641d4d
Natalia Shport <shport.natalia@gmail.com>
2020-09-11 17:34:02 -0700

    Updates

---
+++ precache-manifest.4dac6a53e70826a804ac24dfe2695500.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e0034a3f62d4eb22081dcb5f7b59d8",
+    "url": "/n3218/pet-hospital-app/index.html"
+  },
+  {
+    "revision": "d6415140e6c8c9e7b48e",
+    "url": "/n3218/pet-hospital-app/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "603260fbe31a923e93a9",
+    "url": "/n3218/pet-hospital-app/static/css/main.27763e55.chunk.css"
+  },
+  {
+    "revision": "d6415140e6c8c9e7b48e",
+    "url": "/n3218/pet-hospital-app/static/js/2.12654a98.chunk.js"
+  },
+  {
+    "revision": "e386ada3ad03a9606684b1f649731b40",
+    "url": "/n3218/pet-hospital-app/static/js/2.12654a98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "603260fbe31a923e93a9",
+    "url": "/n3218/pet-hospital-app/static/js/main.6b5a83b5.chunk.js"
+  },
+  {
+    "revision": "c46f983bb2f72bc5b080",
+    "url": "/n3218/pet-hospital-app/static/js/runtime-main.bf9e2767.js"
+  }
+]);
